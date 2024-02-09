48efaee38fc9fea17b35cff3e76bc449231d056d
Rishu Raj <rishu25dec@gmail.com>
2024-02-09 23:40:50 +0530

    added file succes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans = p.product(10,15);
+        int ans1 = p.product(10,15,20);
 
+        double ans2 = p.product(1.2,2.6,9.8);
+        System.out.println(ans+" "+ans2+" "+ans1);
+    }
+
+
+   static class  Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+       public int product(int x, int y, int z) {
+           return x*y*z;
+       }
+       public double product(double x, double y, double z) {
+           return x*y*z;
+       }
+
+    }
 }
